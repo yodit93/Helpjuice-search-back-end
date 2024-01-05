0e7a7a7e2a818f48adf98fdd825344fea4a3b219
yodit93 <yoditabebe9391@gmail.com>
2024-01-05 07:34:57 +0300

    Alow any origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://yodit93.github.io/Helpjuice-search-front-end/"
+    origins "*"
 
     resource "api/*",
       headers: :any,
