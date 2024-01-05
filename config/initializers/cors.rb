a07cd1f8a949f0d8cc420203e6c6f2a2322fc098
yodit93 <yoditabebe9391@gmail.com>
2024-01-05 07:30:26 +0300

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,9 +9,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "https://yodit93.github.io/Helpjuice-search-front-end/"
 
-    resource "*",
+    resource "api/*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 
 end
